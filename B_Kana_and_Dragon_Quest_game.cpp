9d9f5f61b7cee47801edf6e140f8a362ddf6d8e7
18-RAJAT <Rajatjoshi7535@gmail.com>
2022-05-14 14:58:47 +0530

    prog

---
+++ B_Kana_and_Dragon_Quest_game.cpp
@@ -0,0 +1,68 @@
+#include<bits/stdc++.h>
+#define Rajat_Joshi            main;
+#define  CSE             ios_base::sync_with_stdio(false);
+#define Joshi           cin.tie(NULL);
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <sstream>
+#include <queue>
+#include <deque>
+#include <bitset>
+#include <iterator>
+#include <list>
+#include <stack>
+#include <map>
+#include <set>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <limits>
+#include <time.h>
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+using namespace std;
+#define read(type) readInt<type>()
+#define ll long long
+#define nL
+
+
+#define pb push_back
+#define mk make_pair
+#define pii pair<int, int>
+#define a first
+#define b second
+#define vi vector<int>
+#define all(x) (x).begin(), (x).end()
+#define umap unordered_map
+#define uset unordered_set
+#define MOD 1000000007
+#define imax INT_MAX
+#define imin INT_MIN
+#define exp 1e9
+#define sz(x) (int((x).size()))
+int32_t main()
+{
+ios_base::sync_with_stdio(false);
+cin.tie(NULL);
+int ttt; cin >> ttt;
+while(ttt--) {
+//_______________________________________________________//
+Rajat_Joshi CSE;
+int x,n,m;cin>>x>>n>>m;
+int ct=0;
+while((x>10)and(n>0))
+{x=x/2+10;n--;}
+while((x>0)and(m>0))
+{x=x-10;m--;if(x<=0)ct=1;}
+
+if(ct==1){cout<<"YES"<<endl;}
+else{cout<<"NO"<<endl;}
+
+}
+return 0;
+}
