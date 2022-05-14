3fb3f6a4df59d412bbe79ee781ce7938f0750e9e
18-RAJAT <Rajatjoshi7535@gmail.com>
2022-05-14 06:14:59 +0530

    new

--- B_Robots.cpp
+++ B_Robots.cpp
@@ -50,37 +50,27 @@ int32_t main()
 ios_base::sync_with_stdio(false);
 cin.tie(NULL);
 int ttt; cin >> ttt;
+string s[10];
 while(ttt--) {
 //_______________________________________________________//
 Rajat_Joshi CSE;
-	ll n, s = 0;
-    	cin >> n;
-    	vector<long long> arr(n), v1, v2;
-    	for(auto &X: arr){
-    		cin >> X;
-    	}
-    	for (int i = 0; i < n; ++i)
-    	{
-    		if(i & 1) {
-    			v1.push_back(abs(arr[i]));
-    		} else {
-    			v2.push_back(abs(arr[i]));
-    		}
-    	}
-    	sort(v1.begin(), v1.end());
-    	sort(v2.begin(), v2.end());
-
-    	if(v1[v1.size()-1] > v2[0]) {
-    		swap(v1[v1.size() - 1], v2[0]);
-    	}
- 
-    	for(auto x: v2) {
-    		s += x;
-    	}
-    	for(auto x: v1) {
-    		s -= x;
-    	}
-    	cout << s << endl;
+int n,m;
+		int x=10,y=10;
+		cin>>n>>m;
+		for(int i=0;i<n;i++) cin>>s[i];
+		for(int i=0;i<n;i++)
+		{
+			for(int j=0;j<m;j++)
+			{
+				if(s[i][j]=='R') 
+				{
+					x=min(x,i);
+					y=min(y,j);
+				}
+			}
+		}
+		if(s[x][y]=='R') cout<<"YES"<<endl;
+		else cout<<"NO"<<endl;
 }
 return 0;
 }
