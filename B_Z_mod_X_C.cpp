59b8c6c335dda9850461c94c1aaa1931c6e0d4c1
18-RAJAT <Rajatjoshi7535@gmail.com>
2022-05-19 20:56:51 +0530

    Contest

---
+++ B_Z_mod_X_C.cpp
@@ -0,0 +1,67 @@
+#include<bits/stdc++.h>
+#define Rajat_Joshi            main;
+#define  CSE             ios_base::sync_with_stdio(false);
+#define Joshi           cin.tie(NULL);
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <sstream>
+#include <queue>
+#include <deque>
+#include <bitset>
+#include <iterator>
+#include <list>
+#include <stack>
+#include <map>
+#include <set>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <limits>
+#include <time.h>
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+using namespace std;
+#define read(type) readInt<type>()
+#define ll long long
+#define nL
+
+
+#define pb push_back
+#define mk make_pair
+#define pii pair<int, int>
+#define a first
+#define b second
+#define vi vector<int>
+#define all(x) (x).begin(), (x).end()
+#define umap unordered_map
+#define uset unordered_set
+#define MOD 1000000007
+#define imax INT_MAX
+#define imin INT_MIN
+#define exp 1e9
+#define sz(x) (int((x).size()))
+int solve()
+{
+    int a,b,c;cin >>a>>b>>c;
+    cout<<a+b+c<<' '<<b+c<<' '<<c<<'\n';
+    return 0;
+}
+
+signed main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    //_______________________________________________________//
+ //Rajat_Joshi CSE;
+
+    int t;
+    cin >> t;
+    while (t--)
+        solve();
+    return 0;
+}
