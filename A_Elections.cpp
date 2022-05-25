36f4cf4f9cbf9928714fcd6ac560307df2bfa5e1
18-RAJAT <Rajatjoshi7535@gmail.com>
2022-05-25 20:11:14 +0530

    NEW

--- A_Elections.cpp
+++ A_Elections.cpp
@@ -54,7 +54,7 @@ while(ttt--) {
 //_______________________________________________________//
 Rajat_Joshi CSE;
 int a,b,c;cin>>a>>b>>c;
-cout<<max(max(b,c)-a+1,0)<<" "<<max(max(a,c)-b+1,0)<<" "<<max(max(a,b)-c+1,0)<<endl;
+cout<<max(max(b,c)-a+1,0)<<" "<<max(max(a,c)-b+1,0)<<" "<<max(max(a,b)-c+1,0)<<"\n";
 }
 return 0;
 }
