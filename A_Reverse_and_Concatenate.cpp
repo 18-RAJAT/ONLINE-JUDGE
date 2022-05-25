f1997ce738f97533fc0024e715fa7bceff920648
18-RAJAT <Rajatjoshi7535@gmail.com>
2022-05-25 19:14:41 +0530

    NEW

---
+++ A_Reverse_and_Concatenate.cpp
@@ -0,0 +1,64 @@
+#include<bits/stdc++.h>
+#define Rajat_Joshi            main;
+#define  CSE             ios_base::sync_with_stdio(false);
+#define Joshi           cin.tie(NULL);
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <sstream>
+#include <queue>
+#include <deque>
+#include <bitset>
+#include <iterator>
+#include <list>
+#include <stack>
+#include <map>
+#include <set>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <limits>
+#include <time.h>
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+using namespace std;
+#define read(type) readInt<type>()
+#define ll long long
+#define nL
+
+
+#define pb push_back
+#define mk make_pair
+#define pii pair<int, int>
+#define a first
+#define b second
+#define vi vector<int>
+#define all(x) (x).begin(), (x).end()
+#define umap unordered_map
+#define uset unordered_set
+#define MOD 1000000007
+#define imax INT_MAX
+#define imin INT_MIN
+#define exp 1e9
+#define sz(x) (int((x).size()))
+int32_t main()
+{
+ios_base::sync_with_stdio(false);
+cin.tie(NULL);
+int ttt; cin >> ttt;
+int n,k;string s;
+while(ttt--) {
+//_______________________________________________________//
+Rajat_Joshi CSE;
+		cin>>n>>k;
+		cin>>s;
+		int ct=0;
+		for(int i=0;i<n;i++)if(s[i]!=s[n-i-1])ct=1;
+		cout<<(ct&&k?2:1)<<"\n";
+}
+return 0;
+}
