50ad4e4fcf8556f9d2c83c0107d44bd1662d429a
18-RAJAT <Rajatjoshi7535@gmail.com>
2022-05-26 21:36:06 +0530

    26

---
+++ Take_discount_or_Not.cpp
@@ -0,0 +1,78 @@
+#include<bits/stdc++.h>
+#define Rajat_Joshi            main;
+#define  CSE             ios_base::sync_with_stdio(false);
+#define Joshi           cin.tie(NULL);
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <sstream>
+#include <queue>
+#include <deque>
+#include <bitset>
+#include <iterator>
+#include <list>
+#include <stack>
+#include <map>
+#include <set>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <limits>
+#include <time.h>
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+using namespace std;
+#define read(type) readInt<type>()
+#define ll long long
+#define nL
+
+
+#define pb push_back
+#define mk make_pair
+#define pii pair<int, int>
+#define a first
+#define b second
+#define vi vector<int>
+#define all(x) (x).begin(), (x).end()
+#define umap unordered_map
+#define uset unordered_set
+#define MOD 1000000007
+#define imax INT_MAX
+#define imin INT_MIN
+#define exp 1e9
+#define sz(x) (int((x).size()))
+int32_t main()
+{
+ios_base::sync_with_stdio(false);
+cin.tie(NULL);
+int ttt; cin >> ttt;
+while(ttt--) {
+//_______________________________________________________//
+Rajat_Joshi CSE;
+       int n;cin>>n;
+       int x,y;cin>>x>>y;
+        int arr[n];
+        int sum1=0,sum2=0;
+        for(int i=0;i<n;i++)
+        {
+             cin >>arr[i];
+            sum1+=arr[i];
+            if((arr[i]-y)<0)
+            {
+                 sum2+=0;
+            }
+            else
+           {
+               int temp=arr[i]-y;
+                sum2+=temp;
+            }
+           }
+        if(sum1>(sum2+x)){cout <<"COUPON"<< endl;}
+        else{cout <<"NO COUPON"<< endl;}
+    }
+return 0;
+}
---
+++ Whatsapp_Messages.cpp
@@ -0,0 +1,135 @@
+// #include<bits/stdc++.h>
+// #define Rajat_Joshi            main;
+// #define  CSE             ios_base::sync_with_stdio(false);
+// #define Joshi           cin.tie(NULL);
+// #include <iostream>
+// #include <string>
+// #include <vector>
+// #include <algorithm>
+// #include <sstream>
+// #include <queue>
+// #include <deque>
+// #include <bitset>
+// #include <iterator>
+// #include <list>
+// #include <stack>
+// #include <map>
+// #include <set>
+// #include <functional>
+// #include <numeric>
+// #include <utility>
+// #include <limits>
+// #include <time.h>
+// #include <math.h>
+// #include <stdio.h>
+// #include <string.h>
+// #include <stdlib.h>
+// #include <assert.h>
+// using namespace std;
+// #define read(type) readInt<type>()
+// #define ll long long
+// #define nL
+
+
+// #define pb push_back
+// #define mk make_pair
+// #define pii pair<int, int>
+// #define a first
+// #define b second
+// #define vi vector<int>
+// #define all(x) (x).begin(), (x).end()
+// #define umap unordered_map
+// #define uset unordered_set
+// #define MOD 1000000007
+// #define imax INT_MAX
+// #define imin INT_MIN
+// #define exp 1e9
+// #define sz(x) (int((x).size()))
+// int32_t main()
+// {
+// ios_base::sync_with_stdio(false);
+// cin.tie(NULL);
+// int ttt; cin >> ttt;
+// while(ttt--) {
+// //_______________________________________________________//
+// Rajat_Joshi CSE;
+// int n;cin>>n;
+// int cnt=0;
+// cnt += (( n / 5 ) * 2);
+    
+//     n = n % 5;
+
+//     if ( n == 4 ) cnt += 2;
+//     else if ( n == 3 ) cnt += 1;
+//     else if ( n == 2 ) cnt += 1;
+    
+//     cout << cnt << "\n";
+// }
+// return 0;
+// }
+
+
+
+#include<bits/stdc++.h>
+#define Rajat_Joshi            main;
+#define  CSE             ios_base::sync_with_stdio(false);
+#define Joshi           cin.tie(NULL);
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <sstream>
+#include <queue>
+#include <deque>
+#include <bitset>
+#include <iterator>
+#include <list>
+#include <stack>
+#include <map>
+#include <set>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <limits>
+#include <time.h>
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+using namespace std;
+#define read(type) readInt<type>()
+#define ll long long
+#define nL
+
+
+#define pb push_back
+#define mk make_pair
+#define pii pair<int, int>
+#define a first
+#define b second
+#define vi vector<int>
+#define all(x) (x).begin(), (x).end()
+#define umap unordered_map
+#define uset unordered_set
+#define MOD 1000000007
+#define imax INT_MAX
+#define imin INT_MIN
+#define exp 1e9
+#define sz(x) (int((x).size()))
+int32_t main()
+{
+ios_base::sync_with_stdio(false);
+cin.tie(NULL);
+int ttt; cin >> ttt;
+while(ttt--) {
+//_______________________________________________________//
+Rajat_Joshi CSE;
+int x,y,z,st,res;
+cin>>x>>y>>z;
+st=x-y;
+res=st*z;
+cout<<res<<endl;
+}
+return 0;
+}
