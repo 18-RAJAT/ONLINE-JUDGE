a92420b1665a982f4eb6999357a30c31c8a0aac0
18-RAJAT <Rajatjoshi7535@gmail.com>
2025-02-12 08:30:57 +0530

    12/02/25

--- F_Bitwise_Slides.cpp
+++ F_Bitwise_Slides.cpp
@@ -6,36 +6,23 @@ void sol()
 {
     int n;
     cin>>n;
-    vector<int>a(n);
-    for(int i=0;i<n;++i)cin>>a[i];
-    int ans=1;
+    int ans=0,bit=0;
     unordered_map<int,int>mp;
-    mp.reserve(n+10);
-    int res=ans;
+    mp[0]=1;
     for(int i=0;i<n;++i)
     {
-        int tp=a[i],rem=0;
-        if(mp.count(tp))rem=mp[tp]%Mod;
-        unordered_map<int,int>mp1;
-        mp1.reserve(mp.size()+10);
-        for(auto& it:mp)
-        {
-            int F=it.first,S=it.second;
-            (F==tp)?mp1[tp]=(mp1[tp]+(2*S)%Mod)%Mod:mp1[F^tp]=(mp1[F^tp]+S)%Mod;
-            // if(F==tp)rem=(rem+S)%Mod;
-        }
-        //f(S,i)=f(S,i−1)+f(S,i−1)+f(S|(tp),i−1)=3*f(S,i−1)
-        mp1[tp]=(mp1[tp]+(3*ans)%Mod)%Mod,mp1[tp]%=Mod,ans=rem%Mod;
-        //mp1[tp]=(mp1[tp]+ans%Mod)%Mod,mp1[tp]%=Mod,ans=rem%Mod;
-        mp=move(mp1);
-        int tot=accumulate(mp.begin(),mp.end(),0LL,[&](int s,pair<int,int>p){return(s+p.second)%Mod;});
-        res=(ans+tot)%Mod;
+        int x;
+        cin>>x;
+        int Nbit=bit+x-2*(bit&x);
+        mp[bit]=(mp[bit]*3+mp[Nbit]*2)%Mod;
+        bit=move(Nbit);
     }
-    cout<<res%Mod<<endl;
+    for(auto& it:mp)ans=(ans+it.second)%Mod;
+    cout<<ans<<endl;
 }
 signed main()
 {
-    int t;
+    int t;  
     cin>>t;
     while(t--)
     {
