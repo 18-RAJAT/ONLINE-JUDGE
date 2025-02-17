f7ca55c014bb0711eb3a3e380ce3066c89b2cf38
18-RAJAT <Rajatjoshi7535@gmail.com>
2025-02-17 10:04:00 +0530

    17/02/25

--- B_Variety_is_Discouraged.cpp
+++ B_Variety_is_Discouraged.cpp
@@ -7,40 +7,19 @@ void sol()
     cin>>n;
     vector<int>a(n);
     for(int i=0;i<n;++i)cin>>a[i];
-    map<int,int>f,l;
+    map<int,int>mp;
+    for(int i=0;i<n;++i)mp[a[i]]++;
+    int ans=0,l=-1,r=-1,x=-1,cnt=0;
     for(int i=0;i<n;++i)
     {
-        if(!f.count(a[i]))f[a[i]]=i+1;
-        l[a[i]]=i+1;
+        if(mp[a[i]]==1)
+        {
+            if(x==-1)x=i;cnt++;
+            if(ans<cnt)ans=cnt,l=x,r=i;
+        }
+        else x=-1,cnt=0;
     }
-    vector<int>e;
-    for(auto& it:f)e.push_back(it.first);
-    sort(e.begin(),e.end(),[&](int x,int y){return f[x]<f[y];});
-    int m=e.size();
-    vector<int>pref,suf,res(m);
-    for(auto& it:e)pref.push_back(f[it]),suf.push_back(l[it]);
-    res[m-1]=suf[m-1];
-    for(int i=m-2;~i;--i)res[i]=max(suf[i],res[i+1]);
-    int ans=LLONG_MIN,pre=0,nxt=0,x=0,y=0,p=0,q=0;
-    for(int i=0;i<m;++i)
-    {
-        int L=pref[i],R=res[i],cnt=m-i,chk=cnt-(R-L+1),sza=R-L+1;
-        if(ans<chk)
-        {
-            ans=chk,x=sza,pre=L,nxt=R;
-        }
-        else if(chk==ans && sza>x)
-        {
-            x=sza,pre=L,nxt=R;
-        }
-        if(chk==0 && sza>y)
-        {
-            y=sza,p=L,q=R;
-        }
-    }
-    if(ans>0)cout<<pre<<" "<<nxt<<endl;
-    else if(y>0)cout<<p<<" "<<q<<endl;
-    else cout<<0<<endl;
+    (ans>=1?cout<<l+1<<" "<<r+1<<endl:cout<<0<<endl);
 }
 signed main()
 {
