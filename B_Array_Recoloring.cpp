b43869c4dd8cf641ff455a47e6506b248c76df57
18-RAJAT <Rajatjoshi7535@gmail.com>
2025-03-18 09:16:41 +0530

    18/03/25

--- B_Array_Recoloring.cpp
+++ B_Array_Recoloring.cpp
@@ -5,50 +5,19 @@ void sol()
 {
     int n,k;
     cin>>n>>k;
-    vector<int>a(n);
+    vector<int>a(n); 
     for(int i=0;i<n;++i)cin>>a[i];
-    vector<vector<int>>pref(n+1,vector<int>(k+1,-1)),suf(n+2,vector<int>(k+1,-1));
-    pref[0][0]=suf[n+1][0]=0;
-    for(int i=1;i<=n;++i)
+    vector<int>b=a;
+    ranges::sort(a,greater<>());
+    if(k==1)
     {
-        pref[i][0]=0;
-        for(int j=1;j<=min(i,k);++j)
+        if(a[0]!=b[0] && a[0]!=b.back())
         {
-            pref[i][j]=pref[i-1][j];
-            if(~pref[i-1][j-1])pref[i][j]=max(pref[i][j],pref[i-1][j-1]+a[i-1]);
+            cout<<a[0]+max(b[0],b.back())<<endl;
+            return;
         }
     }
-    for(int i=n;i;--i)
-    {
-        suf[i][0]=0;
-        for(int j=1;j<=min(n-i+1,k);++j)
-        {
-            suf[i][j]=suf[i+1][j];
-            if(~suf[i+1][j-1])suf[i][j]=max(suf[i][j],suf[i+1][j-1]+a[i-1]);
-        }
-    }
-    int ans=0;
-    for(int i=1;i<=n;++i)
-    {
-        int tmp=a[i-1],res=-1;
-        if(i==1)
-        {
-            if(n>1 && ~suf[2][k])res=tmp+suf[2][k];
-        }
-        else if(i==n)
-        {
-            if(~pref[n-1][k])res=tmp+pref[n-1][k];
-        }
-        else
-        {
-            for(int j=1;j<k;++j)
-            {
-                if(~pref[i-1][j] && ~suf[i+1][k-j])res=max(res,tmp+pref[i-1][j]+suf[i+1][k-j]);
-            }
-        }
-        ans=max(ans,res);
-    }
-    cout<<ans<<endl;
+    cout<<accumulate(a.begin(),a.begin()+k+1,0LL)<<endl;
 }
 signed main()
 {
