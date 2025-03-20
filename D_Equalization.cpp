9b711b9113fa61d91b7c2bb6d507837ad8e324f2
18-RAJAT <Rajatjoshi7535@gmail.com>
2025-03-20 10:32:11 +0530

    20/03/25

--- D_Equalization.cpp
+++ D_Equalization.cpp
@@ -1,65 +1,41 @@
 #include<bits/stdc++.h>
 using namespace std;
 #define int long long
-const int N=60,NN=(1ULL<<62);
+int dp[105][105];
 void sol()
 {
     int x,y;
     cin>>x>>y;
-    vector<vector<int>>dp(N+1,vector<int>(N+1,NN));
-    dp[0][0]=0;
-    for(int k=1;k<=N;++k)
+    if(x==y)
     {
-        vector<vector<int>>ndp=dp;
-        int mask=(1LL<<k);
-        for(int i=0;i<=N-k;++i)
-        {
-            for(int j=0;j<=N;++j)
-            {
-                if(dp[i][j]!=NN)
-                {
-                    int f=i+k,s=dp[i][j]|mask;
-                    if(s<ndp[f][j])ndp[f][j]=s;
-                }
-            }
-        }
-        for(int i=0;i<=N;++i)
-        {
-            for(int j=0;j<=N-k;++j)
-            {
-                if(dp[i][j]!=NN)
-                {
-                    int f=j+k,s=dp[i][j]|mask;
-                    if(s<ndp[i][f])ndp[i][f]=s;
-                }
-            }
-        }
-        dp=move(ndp);
+        cout<<0<<endl;
+        return;
     }
-    vector<array<int,2>>vp;
-    vector<int>ar;
-    for(int i=0;i<=N;++i)
+    int ans=LLONG_MAX;
+    for(int i=0;i<63;++i)
     {
-        for(int j=0;j<=N;++j)
+        for(int j=0;j<63;++j)
         {
-            if(dp[i][j]!=NN)
-            {
-                vp.push_back({i,j});
-                ar.push_back(dp[i][j]);
-            }
+            if((x>>i)==(y>>j))ans=min(ans,dp[i][j]);
         }
-    }
-    int ans=NN;
-    for(int i=0;i<vp.size();++i)
-    {
-        int f=vp[i][0],s=vp[i][1];
-        // (x & ~((1LL<<f)-1))==(y & ~((1LL<<s)-1));
-        if((x>>f)==(y>>s))ans=min(ans,ar[i]); //&& (x&((1LL<<f)-1))==(y&((1LL<<s)-1)))
     }
     cout<<ans<<endl;
 }
 signed main()
 {
+    memset(dp,0x3F,sizeof(dp));
+    dp[0][0]=0;
+    for(int k=1;k<63;++k)
+    {
+        for(int i=62;~i;--i)
+        {
+            for(int j=62;~j;--j)
+            {
+                if(i>=k)dp[i][j]=min(dp[i][j],dp[i-k][j]+(1LL<<k));
+                if(j>=k)dp[i][j]=min(dp[i][j],dp[i][j-k]+(1LL<<k));
+            }
+        }
+    }
     int t;
     cin>>t;
     while(t--)
