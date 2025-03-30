88e347f3ca8f8135b12096ee62e272deb12f62fe
18-RAJAT <Rajatjoshi7535@gmail.com>
2025-03-30 07:44:44 +0530

    30/03/25

---
+++ A_Kamilka and the Sheep.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+void sol()
+{
+    int n;
+    cin>>n;
+    vector<int>a(n);
+    for(int i=0;i<n;++i)cin>>a[i];
+    cout<<*max_element(a.begin(),a.end())-*min_element(a.begin(),a.end())<<endl;
+}
+signed main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        sol();
+    }
+    return 0;
+}
