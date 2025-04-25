f9494ba80df5ce8968fed32cf4710fa03f48e93c
18-RAJAT <Rajatjoshi7535@gmail.com>
2025-04-25 10:46:12 +0530

    25/04/25

---
+++ A_Dr_TC.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+void sol()
+{
+    int n;
+    cin>>n;
+    string s;
+    cin>>s;
+    int ans=0;
+    for(auto& it:s)
+    {
+        (it=='1')?ans+=n-1:ans++;
+    }
+    cout<<ans<<endl;
+}
+signed main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        sol();
+    }
+    return 0;
+}
