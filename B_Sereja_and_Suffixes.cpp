4bd0a5424bce7382254ee27f03698ffbb012f133
18-RAJAT <Rajatjoshi7535@gmail.com>
2025-05-02 09:50:12 +0530

    02/05/25

---
+++ B_Sereja_and_Suffixes.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+void sol()
+{
+    int n,m;
+    cin>>n>>m;
+    vector<int>a(n),ans(n);
+    for(int i=0;i<n;++i)cin>>a[i];
+    unordered_set<int>s;
+    ans[n-1]=1;
+    s.insert(a[n-1]);
+    for(int i=n-2;~i;--i)
+    {
+        if(s.count(a[i]))
+        {
+            ans[i]=ans[i+1]+1;
+            s.insert(a[i]);
+        }
+        else ans[i]=ans[i+1];
+    }
+    while(m--)
+    {
+        int x;
+        cin>>x;
+        cout<<ans[x-1]<<endl;
+    }
+}
+signed main()
+{
+    sol();
+    return 0;
+}
