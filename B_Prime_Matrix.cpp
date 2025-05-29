a7172a17008978e33ce3b85cec873cd271edefa8
18-RAJAT <Rajatjoshi7535@gmail.com>
2025-05-29 15:34:23 +0530

    29/05/25

--- B_Prime_Matrix.cpp
+++ B_Prime_Matrix.cpp
@@ -1,521 +1,58 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
- 
-#define ll long long
-#define sza(x) ((int)x.size())
-#define all(x) x.begin(), x.end()
-#define revall(x) x.rbegin(), x.rend()
-#define sortall(x) sort(all(x))
- 
-const int MAX_N = 2e5 + 5;
-const int MAX_NN = 2e5 + 8;
-const ll MOD = 1e9 + 7;
-const ll INF = 1e18+20;
-#define revall(x) x.rbegin(), x.rend()
-#define ALL(x) sort(x.begin(), x.end())
-#define sortall(x) sort(all(x))
-#define reverseall(x) reverse(all(x))
-
-
-
-///////////////////////////////////////////////////////////////////////////////////////////////////
-
-#define SCD(t) scanf("%d",&t)
-#define SCLD(t) scanf("%ld",&t)
-#define SCLLD(t) scanf("%lld",&t)
-#define SCC(t) scanf("%c",&t)
-#define SCS(t) scanf("%s",t)
-#define SCF(t) scanf("%f",&t)
-#define SCLF(t) scanf("%lf",&t)
-#define MEM(a, b) memset(a, (b), sizeof(a))
-
-#define FOR(i, j, k, in) for (ll i=j ; i<k ; i+=in)
-#define RFOR(i, j, k, in) for (ll i=j ; i>=k ; i-=in)
-#define rall(cont) cont.end(), cont.begin()
-#define FOREACH(it, l) for (auto it = l.begin(); it != l.end(); it++)
-#define IN(A, B, C) assert( B <= A && A <= C)
-
-
-
-///////////////////////////////////////////////////////////////////////////////////////////////////
-
-#define MP make_pair
-#define PB push_back
-#define PF push_front
-#define INF (int)1e9
-#define EPS 1e-9
-// #define MOD 998244353 
-#define ff first
-#define ss second
-#define PI 3.1415926535897932384626433832795
-#define read(type) readInt<type>()
-const double pi=acos(-1.0);
-#define ll long long
-typedef pair<int,int> PII;
-typedef vector<long long> VI;
-typedef vector<bool> VB;
-typedef vector<char> VC;
-typedef vector<char> VC;
-typedef vector<vector<long long>> VII;
-typedef vector<pair<ll,ll>>VPII;
-typedef vector<string> VS;
-typedef vector<PII> VIII;
-typedef vector<VI> VVI;
-// typedef vector<string> VS;
-typedef map<ll,ll> MPII;
-typedef map<char,int> MPCI;
-typedef map<string,int> MPSI;
-typedef set<long long> seti;
-typedef multiset<int> MSETI;
-typedef priority_queue<pair<int,int>> PQPII;
-typedef long int int32;
-typedef unsigned long int uint32;
-typedef long long int int64;
-typedef unsigned long long int  uint64;
-#define nl endl
- 
- 
-/* clang-format off */
- 
-/* TYPES  */
-#define ll long long
-#define pii pair<int, int>
-#define pll pair<long long, long long>
-#define vvi vector<vector<int>>
-#define vll vector<long long>
-#define vd vector<double>
-#define mii map<long,long>
-#define MPC map<char,int> 
-#define miid map<double,long>
-#define si set<int>
-#define sc set<char>
-#define vi vector<int>
-#define revs(x,y,z) for(ll i=x;i<=y;i+=z)
-
-
-//////////////////////////////////////////////////////////////////////////////////////////////
-
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-/* FUNCTIONS */
-
-
-
-#define f1(i,s,e) for(long long int i=s;i<e;i++)
-#define ff1(i,s,e) for(long long int i=s;i>=e;--i)
-#define For1(i,n) (long long int i=0;i<n+1;++i)
-#define cf(i,s,e) for(long long int i=s;i<=e;i++)
-#define FO(i,s,e) for(long long int i=1;i*i<=y;i++)
-#define rf(i,e,s) for(long long int i=e-1;i>=s;i--)
-#define pass(a)  for(long long int i=n-1;i>=1;i-=2)
-#define pb push_back
-#define ppb pop_back
-#define eb emplace_back
-
-
-//////////////////////////////////////////////////////////////////////////////////////////////////////
-
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-
-
-
-
-// ----------------------</BITWISE>-------------------------- 
-/* a=target variable, b=bit number to act upon 0-n */
-#define BIT_SET(a,b) ((a) |= (1ULL<<(b)))
-#define BIT_CLEAR(a,b) ((a) &= ~(1ULL<<(b)))
-#define BIT_FLIP(a,b) ((a) ^= (1ULL<<(b)))
-
-// '!!' to make sure this returns 0 or 1
-#define BIT_CHECK(a,b) (!!((a) & (1ULL<<(b))))
-
-#define BITMASK_SET(x, mask) ((x) |= (mask))
-#define BITMASK_CLEAR(x, mask) ((x) &= (~(mask)))
-#define BITMASK_FLIP(x, mask) ((x) ^= (mask))
-#define BITMASK_CHECK_ALL(x, mask) (!(~(x) & (mask)))
-#define BITMASK_CHECK_ANY(x, mask) ((x) & (mask))
-// ----------------------</BITWISE END>--------------------------
-
-
-
-
-
-/****************** Prime Generator **********************/ 
-// const int N=1e7+10; int prime[20000010]; 
-// bool isprime[N]; int nprime; 
-// template <typename T> void Sieve(T a) 
-// {nprime = 0;memset(isprime,true,sizeof(isprime));
-// isprime[1]=false;for(int i=2;i<N;i++){
-// if(isprime[i]){prime[nprime++]=i;for(int j=2;i*j<N;j++)
-// isprime[i*j]=false;}}}
-// template <typename T> inline T PrimeFactors(T n)
-// {ll cnt=0,sum=1;for(int i=0; prime[i]*prime[i]<=n 
-// and i<nprime;i++){cnt=0;while(n%prime[i]==0)
-// {cnt++;n/=prime[i];}sum*=(cnt+1);}
-// if(n>1)sum*=2;return sum;} 
-/**************** Prime Generator End ********************/
-
-
-
-
-
-
-/****************** Geometry *****************/ 
-template <typename T> inline T PointDistanceHorVer(T x1,T y1,T x2, T y2) 
-{return abs(x1-x2)+abs(y1-y2);} 
-template <typename T> inline T PointDistanceDiagonally(T x1,T y1,T x2, T y2) 
-{return sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1));} 
-template <typename T> inline T PointDistanceMinimum(T x1,T y1,T x2, T y2) 
-{ T tmp1=abs(x1-x2); T tmp2=abs(y1-y2); T tmp3=abs(tmp1-tmp2); 
-T tmp4=min(tmp1, tmp2); return tmp3+tmp4; } 
-template <typename T> inline T PointDistance3D(T x1,T y1,T z1,T x2,T y2,T z2)
-{return sqrt(square(x2-x1)+square(y2-y1)+square(z2-z1));} 
- 
-template <typename T> inline T Cube(T a){return a*a*a;} 
-template <typename T> inline T RectengularPrism(T a,T b,T c)
-{return a*b*c;}
-template <typename T> inline T Pyramid(T base, T height)
-{return (1/3)*base*height;}
-template <typename T> inline T Ellipsoid(T r1,T r2,T r3) 
-{return (4/3)*PI*r1*r2*r3;} 
-template <typename T> inline T IrregualarPrism(T base, T height)
-{return base*height;} 
-template <typename T> inline T Sphere(T radius)
-{ return (4/3)*PI*radius*radius*radius;} 
-template <typename T> inline T CylinderB(T base, T height)
-{return base*height;} // base and height 
-template <typename T> inline T CylinderR(T radius, T height)
-{return PI*radius*radius*height;} // radius and height 
-template <typename T> inline T Cone (T radius,T base, T height)
-{return (1/3)*PI*radius*radius*height;} 
-/****************** Geometry end *****************/ 
-
-
-/******* Debugging Class Template *******/
-
-
-    #define debug(args...)     (Debugger()) , args
-
-    class Debugger
+#define int long long
+int prime[100105];
+void sieve(int n)
+{
+    for(int i=1;i<=n;++i)prime[i]=1;
+    prime[1]=0;
+    for(int i=2;i<=n;++i)
     {
-        public:
-        Debugger(const std::string& _separator = " - ") :
-        first(true), separator(_separator){}
-
-        template<typename ObjectType> Debugger& operator , (const ObjectType& v)
+        if(prime[i])
         {
-            if(!first)
-                std:cerr << separator;
-            std::cerr << v;
-            first = false;
-            return *this;
+            for(int j=i*i;j<=n;j+=i)prime[j]=0;
         }
-        ~Debugger() {  std:cerr << endl;}
-
-        private:
-        bool first;
-        std::string separator;
-    };
-
-//////////////////////////////////////////////////////////////////////////////////////////////////////
-
-/* PRINTS */
-template <class T>
-void print_v(vector<T> &v) { cout << "{"; for (auto x : v) cout << x << ","; cout << "\b}"; }
- 
-/* UTILS */
-ll min(ll a,int b) { if (a<b) return a; return b; }
-ll min(int a,ll b) { if (a<b) return a; return b; }
-ll max(ll a,int b) { if (a>b) return a; return b; }
-ll max(int a,ll b) { if (a>b) return a; return b; }
-ll gcd(ll a,ll b) { if (b==0) return a; return gcd(b, a%b); }
-ll lcm(ll a,ll b) { return a/gcd(a,b)*b; }
-string to_upper(string a) { for (int i=0;i<(int)a.size();++i) if (a[i]>='a'  and  a[i]<='z') a[i]-='a'-'A'; return a; }
-string to_lower(string a) { for (int i=0;i<(int)a.size();++i) if (a[i]>='A'  and  a[i]<='Z') a[i]+='a'-'A'; return a; }
- 
- 
-void yes() { cout<<"YES\n"; }
-void no() { cout<<"NO\n"; }
-bool isSorted(vi &arr){for(int i=0;i<(int)arr.size()-1;i++) if(arr[i] >= arr[i+1]) return false;return true;}
- 
- 
- 
-/*  All Required define Pre-Processors and typedef Constants */
-typedef long int int32;
-typedef unsigned long int uint32;
-typedef long long int int64;
-typedef unsigned long long int  uint64;
- 
-bool isprime(ll n)
-{
-    ll x=sqrt(n);
-    for(int i=2;i<=x;i++)
-    {if(n%i==0)return true;}
-    return false;
+    }
 }
- 
- 
-bool cmp(pair<int,int>x,pair<int,int>y){
-    return x.second<y.second;
-}
- 
- 
-void two_D_PrifixSum() 
+void sol()
 {
-    //prefix sum in 2 D array
-    int n,m;cin>>n>>m;
-    int a[n][m],prefix[n][m];
-    f1(i,0,n)
+    int n,m;
+    cin>>n>>m;
+    vector<vector<int>>a(n,vector<int>(m));
+    for(int i=0;i<n;++i)
     {
-        f1(j,0,m)
+        for(int j=0;j<m;++j)cin>>a[i][j];
+    }
+    int ans=1e18;
+    for(int i=0;i<n;++i)
+    {
+        int cnt=0;
+        for(int j=0;j<m;++j)
         {
-            cin>>a[i][j];
+            int k=0,x=a[i][j];
+            while(!prime[a[i][j]]){k++;a[i][j]++;}
+            a[i][j]=x;
+            cnt+=k;
         }
+        ans=min(ans,cnt);
     }
- 
-    //create a prefix array
-    f1(i,0,n)
+    for(int j=0;j<m;++j)
     {
-        f1(j,0,m)
+        int cnt=0;
+        for(int i=0;i<n;++i)
         {
-            prefix[i][j]=a[i][j];
-            if(i-1>=0)prefix[i][j]+=prefix[i-1][j];
-            if(j-1>=0)prefix[i][j]+=prefix[i][j-1];
- 
-            if(i-1>=0 and j-1>=0)
-            {
-                prefix[i][j]-=prefix[i-1][j-1];
-            }
+            int k=0,x=a[i][j];
+            while(!prime[a[i][j]]){k++;a[i][j]++;}
+            a[i][j]=x;
+            cnt+=k;
         }
+        ans=min(ans,cnt);
     }
-    int q;cin>>q;
-    while(q--)
-    {
-        int i1,i2,j1,j2;cin>>i1>>i2>>j1>>j2;
-    }
+    cout<<ans;
 }
- 
- 
-//Scanline algorithm
-void scanlineAlgoritm()
+signed main()
 {
-    int n;cin>>n;
-    int a[n];
-    f1(i,0,n)cin>>a[i];
- 
-    int prefixSum[n+1];
-    int q;cin>>q;
-    while(q--)
-    {
-        int l,r,x;cin>>l>>r>>x;
-        prefixSum[l]+=x;
-        prefixSum[r+1]-=x;
-    }
-    int s=0;
-    f1(i,0,n)
-    {
-        s+=prefixSum[i];
-        a[i]+=s;
-    }
-    f1(i,0,n)
-    {
-        cout<<a[i]<<" ";
-    }
-}
-
-
-int findMin(vector<bool>&vis,VI distance)
-{
-    int start=-1;
-    int n=vis.size();
-    f1(i,0,n)
-    {
-        if(start==-1 or distance[start]>distance[i])
-            start=i;
-    }
-    return start;
-}
-
-int dist(VPII vis,int n)
-{
-    vector<bool>ar(n,false);
-    VI temp(n,INF);
-    ar[1]=0;
-
-    int val=findMin(ar,temp);
-    if(val==-1)return 0;
-    ar[val]=true;
-    cf(i,0,n)
-    {
-        return temp[n];
-    }
-}
-
-
-bool binarySearch(ll mid,ll k,ll x)
-{
-    ll tot=0;
-    mid-=1;
-    if(mid<=k)
-        tot=(mid*(mid+1))/2;
-    else
-    {
-        tot=(k*(k+1))/2;
-        mid=2*k-1-mid;
-        tot+=((k*(k-1))/2-(mid*(mid+1))/2);
-    }
-    return tot<x;
-}
-
-
-
-vi helper(int n)
-{
-    vi v;
-    for (int i = 1; i <=sqrt(n); i++) 
-    {
-        if (n % i == 0) {
-            if (n / i == i)
-            {
-                v.push_back(i);
-            }else
-            {
-                v.push_back(n/i);
-            }
-        }
-    }
-    return v;
-}
-
-
-
-ll dmod(ll x)
-{
-    return ((x+1000000007)%1000000007);
-}
-
-ll take(ll x,ll y)
-{
-    ll ans=1;
-    while(y)
-    {
-        if(y&1)ans=dmod(ans*x);
-        y/=2;
-        // x=dmod(sqrt(x));
-        x=dmod(x*x);
-    }
-    return ans;
-}
-
-ll help( long long x )
-{
-    ll sum = 0 ;
-    while ( x )
-    { 
-        sum += x % 10 ;
-        x /= 10 ;
-    }
-    return sum ;
-}
- 
-
-bool c2(pair<string,pair<int,int>> &x,pair<string,pair<int,int>>&y)
-{
-    if(x.second.first!=y.second.first)
-     return (x.second.first < y.second.first);
-     else
-     return (x.second.second < y.second.second);
-
-}
-bool c1(pair<string,pair<int,int>> &x,pair<string,pair<int,int>>&y)
-{
-    if(x.first!=y.first)
-     return (x.first < y.first);
-     else
-      return (x.second.second < y.second.second);
-
-}
-bool isPrime(int n)
-{
-    // Corner case
-    if (n <= 1)
-        return false;
-    for (int i = 2; i < n; i++)
-        if (n % i == 0)
-            return false;
- 
-    return true;
-}
-
-int ft(char c)
-{
-    return c - 'a';
-}
-char fr(int i)
-{
-    return i+'a';
-}
-
-
-class Solution {
-public:
-void solve()
-{
-    int n,m;cin>>n>>m;
-    int a[n][m];
-    f1(i,0,n)
-    {
-        f1(i,0,m)
-        {
-            cin>>a[i][j];
-        }
-    }
-    int row[n];
-    memset(row,0,sizeof(row));
-    
-    int col[m];
-    memset(col,0,sizeof(col));
-
-    f1(i,0,n)
-    {
-        f1(j,0,m)
-        {
-            int primeMatrix=*
-            // if(a[i][j]==1)
-            // {
-            //     row[i]=1;
-            //     col[j]=1;
-            // }
-        }
-    }
-}
-};
-
-int main() {
-    ios_base::sync_with_stdio(0);
-    cin.tie(0); cout.tie(0);
-// #ifndef ONLINE_JUDGE
-// freopen("input.txt","r",stdin); //file input.txt is opened in reading mode i.e "r"
-// freopen("output.txt","w",stdout);  //file output.txt is opened in writing mode i.e "w"
-// #endif
-    ll tc = 1;
-    // cin >> tc;
-    for (ll t = 1; t <= tc; t++) {
-    // //cout << "Case #" << t << ": ";
-    Solution s;
-    //     if(s.solve())
-    //     {
-    //         // cout<<"Yes"<<nl;
-    //     }
-    //     else
-    //     {
-    //         // cout<<"No"<<nl;
-    //     }
-    // }
-        // solve();
-        // Solution s;
-        s.solve();
-    }
+    sieve(100105);
+    sol();
     return 0;
 }
