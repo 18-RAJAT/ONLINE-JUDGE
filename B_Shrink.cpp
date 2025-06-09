0120a2e2ea03728c9f2f7dbfd3411016ede01d43
18-RAJAT <Rajatjoshi7535@gmail.com>
2025-06-10 00:44:27 +0530

    10/06/25

---
+++ B_Shrink.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+void sol()
+{
+    int n;
+    cin>>n;
+    cout<<1<<" ";
+    for(int i=3;i<=n;++i)cout<<i<<" ";
+    cout<<2<<endl;
+}
+signed main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        sol();
+    }
+    return 0;
+}
