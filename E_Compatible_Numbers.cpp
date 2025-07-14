e7c4bb2e4ac60f35ba653350e6fd5c5d884af4de
18-RAJAT <Rajatjoshi7535@gmail.com>
2025-07-14 10:35:58 +0530

    14/07/25

--- E_Compatible_Numbers.cpp
+++ E_Compatible_Numbers.cpp
@@ -1,15 +1,8 @@
 #include<bits/stdc++.h>
 using namespace std;
 #define int long long
-const int N=(1<<22)+2;
+const int N=(1<<22)+5;
 int dp[N];
-void recur(int mask,int val)
-{
-    if(mask>=N)return;
-    if(~dp[mask])return;
-    dp[mask]=val;
-    for(int i=0;i<22;++i)recur((mask|(1<<i)),val);
-}
 void sol()
 {
     int n;
@@ -17,8 +10,30 @@ void sol()
     vector<int>a(n);
     for(int i=0;i<n;++i)cin>>a[i];
     memset(dp,-1,sizeof(dp));
-    for(auto& it:a)recur(it,it);
-    for(int i=0;i<n;++i)cout<<dp[(~a[i])&((1<<22)-1)]<<" ";
+    queue<int>q;
+    for(auto& it:a)
+    {
+        if(dp[it]==-1)
+        {
+            dp[it]=it;
+            q.push(it);
+        }
+    }
+    while(!q.empty())
+    {
+        int mask=q.front();
+        q.pop();
+        for(int i=0;i<22;++i)
+        {
+            int Nmask=mask|(1<<i);
+            if(Nmask<N && dp[Nmask]==-1)
+            {
+                dp[Nmask]=dp[mask];
+                q.push(Nmask);
+            }
+        }
+    }
+    for(int i=0;i<n;++i)cout<<dp[((1<<22)-1)^a[i]]<<" ";
     cout<<endl;
 }
 signed main()
