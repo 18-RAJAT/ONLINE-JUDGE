6a59302f790de3e6debe1889186ad545cd552559
18-RAJAT <rajatjoshi7535@gmail.com>
2025-08-13 10:41:31 +0530

    13/08/25

---
+++ E_Polycarp_s_New_Job.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+int mx=0,mn=0;
+void sol()
+{
+    int l,r;
+    char ch;
+    cin>>ch>>l>>r;
+    if(ch=='+')mx=max(mx,max(l,r)),mn=max(mn,min(l,r));
+    else
+    {
+        cout<<(((mx<=l && mx<=r) || (mx<=r && mn<=l))?"YES":"NO")<<endl;
+    }
+}
+signed main()
+{
+    int t;cin>>t;
+    while(t--)
+    {
+        sol();
+    }
+    return 0;
+}
