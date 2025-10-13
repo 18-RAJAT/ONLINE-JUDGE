58fc3ed6239df450d39a313695258696a59b4041
18-RAJAT <rajatjoshi7535@gmail.com>
2025-10-13 13:34:55 +0530

    13/10/25

---
+++ A_MEX_Partition.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+void sol()
+{
+    int n;
+    cin>>n;
+    vector<int>a(n);
+    for(int i=0;i<n;++i)cin>>a[i];
+    int ans=0;
+    set<int>st(a.begin(),a.end());
+    while(st.count(ans))ans++;
+    cout<<ans<<endl;
+}
+signed main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        sol();
+    }
+    return 0;
+}
