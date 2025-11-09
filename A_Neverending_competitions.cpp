670bbc1d4f454bc05b528e5bcade35fbcf5d8fc1
Rajat Joshi <rajat@example.com>
2025-11-09 06:10:45 +0530

    09/11/25

---
+++ A_Neverending_competitions.cpp
@@ -0,0 +1,14 @@
+#include"bits/stdc++.h"
+using namespace std;
+#define int long long
+void sol()
+{
+    int n;
+    cin>>n;
+    cout<<((n%2==0)?"home":"contest");
+}
+signed main()
+{
+    sol();
+    return 0;
+}
