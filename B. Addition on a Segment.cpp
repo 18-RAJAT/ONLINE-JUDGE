3bf15096112c4915b81c0f2079b7e1c948962aaf
Rajat Joshi <rajat@example.com>
2025-12-03 11:44:15 +0530

    03/12/25

---
+++ B. Addition on a Segment.cpp
@@ -0,0 +1,27 @@
+#include"bits/stdc++.h"
+using namespace std;
+#define int long long
+void sol()
+{
+    int n;
+    cin>>n;
+    int sum=0,res=0;
+    vector<int>a(n);
+    for(int i=0;i<n;++i)
+    {
+        cin>>a[i];
+        sum+=a[i];
+        if(a[i]==0)res++;
+    }
+    cout<<min(res,sum-n+1)<<endl;
+}
+signed main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        sol();
+    }
+    return 0;
+}
