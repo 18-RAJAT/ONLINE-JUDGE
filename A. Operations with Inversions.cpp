3e08a0a7974cb7f0f8a93b9231c9fd25bb58dd30
Rajat Joshi <rajat@example.com>
2025-12-12 12:12:31 +0530

    12/12/2025

---
+++ A. Operations with Inversions.cpp
@@ -0,0 +1,26 @@
+#include"bits/stdc++.h"
+using namespace std;
+#define int long long
+void sol()
+{
+    int n;
+    cin>>n;
+    vector<int>a(n);
+    int ans=0,mx=0;
+    for(auto& it:a)
+    {
+        cin>>it;
+        (mx<it)?mx=it:ans++;
+    }
+    cout<<ans<<endl;
+}
+signed main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        sol();
+    }
+    return 0;
+}
