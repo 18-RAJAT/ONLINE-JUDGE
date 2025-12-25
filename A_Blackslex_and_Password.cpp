87c6809e56f8b04a30a2e7c72a9fbf9bbbd4cdec
Rajat Joshi <rajat@example.com>
2025-12-25 10:01:15 +0530

    25/12/2025

---
+++ A_Blackslex_and_Password.cpp
@@ -0,0 +1,19 @@
+#include"bits/stdc++.h"
+using namespace std;
+#define int long long
+void sol()
+{
+    int k,x;
+    cin>>k>>x;
+    cout<<(k*x)+1<<endl;
+}
+signed main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        sol();
+    }
+    return 0;
+}
