b9bbb5e4d1dd001ca520f1d8df922fc493c0d1f2
Rajat Joshi <rajat@example.com>
2026-01-13 13:06:00 +0530

    13/01/2026

---
+++ A. Mahmoud and Ehab and the even-odd game.cpp
@@ -0,0 +1,14 @@
+#include"bits/stdc++.h"
+using namespace std;
+#define int long long
+void sol()
+{
+    int n;
+    cin>>n;
+    cout<<((n&1)?"Ehab":"Mahmoud");   
+}
+signed main()
+{
+    sol();
+    return 0;
+}
