984dc6e666eacbf0584f11843153073996756323
Rajat Joshi <rajat@example.com>
2026-01-19 10:59:30 +0530

    19/01/2026

---
+++ A. Perfect Root.cpp
@@ -0,0 +1,20 @@
+#include"bits/stdc++.h"
+using namespace std;
+#define int long long
+void sol()
+{
+    int n;
+    cin>>n;
+    for(int i=1;i<=n;++i)cout<<i<<" ";
+    cout<<endl;
+}
+signed main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        sol();
+    }
+    return 0;
+}
